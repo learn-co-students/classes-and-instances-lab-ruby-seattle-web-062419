9d9c9c1f13214e0067317250a6ce31a0e7f8b26f
github username <github email address>
2019-06-25 12:37:45 -0700

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,12 @@
+# Defines class definition of Dog
+class Dog
+end 
+
+# Creates 3 dogs and defines local variables for instances
+# of fido, snoopy, lassie
+    fido = Dog.new
+    fido
+    snoopy = Dog.new
+    snoopy
+    lassie = Dog.new
+    lassie
---
+++ lib/person.rb
@@ -0,0 +1,7 @@
+# Defines a Person class
+class Person
+end
+
+# Creates two instances of People
+alan_kay = Person.new
+adele_goldberg = Person.new
